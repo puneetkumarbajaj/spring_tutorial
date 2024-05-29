278d1f393dc9f4d465edbe41e270447bbbee3084
Puneet Bajaj <puneetbajaj591@gmail.com>
2024-05-29 11:38:33 -0400

    initial commit and mongo connection and model setup

---
+++ src/main/java/com/movieTutorial/movies/Movie.java
@@ -0,0 +1,27 @@
+package com.movieTutorial.movies;
+
+import org.bson.types.ObjectId;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genre;
+    private List<String> backdrops;
+}
---
+++ src/main/java/com/movieTutorial/movies/Review.java
@@ -0,0 +1,20 @@
+package com.movieTutorial.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+    
+}
